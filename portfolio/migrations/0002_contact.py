609c1a4bdf1a8ff9094bb51a3ddb5fd946e98ebb
FIRST_NAME LAST_NAME <kibomamoses100@gmail.com>
2021-01-05 06:38:08 +0300

    changes made to the project

--- portfolio/migrations/0002_contact.py
+++ portfolio/migrations/0002_contact.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
             name='Contact',
             fields=[
 
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+
                 ('name', models.CharField(max_length=196)),
                 ('email', models.EmailField(max_length=254)),
                 ('subject', models.CharField(max_length=196)),
