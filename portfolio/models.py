165d2e3631bb9b31d4827181dd3eb5f61ef1ae24
FIRST_NAME LAST_NAME <kibomamoses100@gmail.com>
2021-01-05 06:30:47 +0300

    changes made to the project

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 class Contact(models.Model):
+    name = models.CharField(max_length=196)
     email = models.EmailField()
     subject = models.CharField(max_length=196)
     message = models.TextField()
--- portfolio/views.py
+++ portfolio/views.py
@@ -12,12 +12,12 @@ def portfolio_page(request):
     
 def contact(request):
     if request.method == 'POST':
-
+        name_r = request.POST.get('name')
         email_r = request.POST.get('email')
         subject_r = request.POST.get('subject')
         message_r = request.POST.get('message')
 
-        c = Contact(email=email_r, subject=subject_r, message=message_r)
+        c = Contact(name_r,email=email_r, subject=subject_r, message=message_r)
         c.save()
 
         return render(request, 'thank.html')
